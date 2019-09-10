bda706725274244e3444081becad2973acfbf420
weishoufeng <weishoufeng@vip.qq.com>
2019-09-10 19:15:27 +0800

    跟新了manager

--- script/lib/jinja_filter.py
+++ script/lib/jinja_filter.py
@@ -10,6 +10,10 @@ def first_upper(source):
 def first_lower(source):
     return source[0].lower() + source[1:]
 
+def second_lower(source):
+    return source[0:2].lower() + source[2:]
+
 
 environment.DEFAULT_FILTERS['first_upper'] = first_upper
 environment.DEFAULT_FILTERS['first_lower'] = first_lower
+environment.DEFAULT_FILTERS['second_lower'] = second_lower
